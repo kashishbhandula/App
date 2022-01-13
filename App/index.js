9903727074065611cd83c73c0d0b662cf680b658
kashishbhandula <kashishbhandula112@gmail.com>
2022-01-13 16:51:07 +0530

    View Engine Completed

--- App/index.js
+++ App/index.js
@@ -1,8 +1,19 @@
 const port =3000;
 const express=require('express');
 const app=express();
+
+
+app.set('view engine','ejs');
+app.set('views','./view');
+
+app.get('/',function(req,res){
+    //res.end('<h1>Hello</h1>')
+    res.render('home');
+    return;
+})
+
 app.listen(port,function(err)
 {
     if(err){console.log("Server is not listening");return;}
-    console.log("Server is listening on port",port);
+    console.log(`Server is listening on port:${port}`);
 })
