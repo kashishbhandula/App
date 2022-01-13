8a53ed80cd278279ea44543e66aa5d7c5396a213
kashishbhandula <kashishbhandula112@gmail.com>
2022-01-13 16:20:58 +0530

    Starting with View Engine

--- App/index.js
+++ App/index.js
@@ -6,7 +6,3 @@ app.listen(port,function(err)
     if(err){console.log("Server is not listening");return;}
     console.log("Server is listening on port",port);
 })
-app.get('/',function(req,res){
-
-    res.end('<h1>Express Server is Listening</h1>')
-});
