0760a9fa06d01a8c1c4893db9d3cf92aecc436c1
kashishbhandula <kashishbhandula112@gmail.com>
2022-01-14 14:53:20 +0530

    Controller set up completed

--- App/router/index.js
+++ App/router/index.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const router=express.Router();
-
+const home_controller=require('../controller/home_controller.ejs');
 console.log("router is working fine");
+
+router.get('/',home_controller.home);
 
 
 
