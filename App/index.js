a2529134eb83aff11d66c767328b682c42a37ae5
kashishbhandula <kashishbhandula112@gmail.com>
2022-01-18 21:45:03 +0530

    Databse connectivity completed completely

--- App/index.js
+++ App/index.js
@@ -27,8 +27,8 @@ app.set('layout extraScripts', true);
 app.use('/',require('./router'));
 
 
-app.listen(port,function(err)
+app.listen(port,function(err)   
 {
     if(err){console.log("Server is not listening");return;}
     console.log(`Server is listening on port:${port}`);
-})
+});
