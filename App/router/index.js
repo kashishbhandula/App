1292e7e2352869ad938cec7f441b6536acece612
kashishbhandula <kashishbhandula112@gmail.com>
2022-01-23 15:36:17 +0530

    Authentication half done

--- App/router/index.js
+++ App/router/index.js
@@ -7,7 +7,8 @@ console.log("router is working fine");
 router.get('/',home_controller.home);
 router.get('/sign_up',login_controller.sign_up);
 router.post('/create_profile',login_controller.data_collect);
-
+router.get('/sign_in',login_controller.sign_in);
+router.post('/create_session',login_controller.createSession);
 
 
 
